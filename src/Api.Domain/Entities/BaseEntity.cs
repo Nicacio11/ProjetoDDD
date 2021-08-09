11dbff5ba716ccb77bb2f147c4a8747d70a9064e
Vitor Nicacio <vitornicacio10@hotmail.com>
2021-08-09 16:47:10 -0300

    fix: :recycle: remove unnecessary coupling

--- src/Api.Domain/Entities/BaseEntity.cs
+++ src/Api.Domain/Entities/BaseEntity.cs
@@ -1,11 +1,8 @@
 using System;
-using System.ComponentModel.DataAnnotations;
-
 namespace Api.Domain.Entities
 {
     public abstract class BaseEntity
     {
-        [Key]
         public Guid Id { get; set; }
         private DateTime? _createdAt;
         public DateTime? CreatedAt
